a191ca56c1b7e8d01644c2d056db08fb381ce635
sebastianloschert <sebastian.loschert@gmx.de>
2020-07-21 13:57:24 +0200

    adding test file

---
+++ test_scrape_images.py
@@ -0,0 +1,12 @@
+"""
+test file for fridge detection file
+"""
+import pytest
+from scrape_images import scrape_from_imagenet
+from scrape_images import scrape_from_google
+
+def test_scrape_from_imagenet():
+    pass
+
+def test_creape_from_google():
+    pass
