f27414bf55734a614ffd87f5975b5c83c27019f0
sebastianloschert <sebastian.loschert@gmx.de>
2020-07-21 13:57:37 +0200

    deleting old test file

--- testing/test_scrape_images.py
+++
@@ -1,9 +0,0 @@
-"""
-test file for fridge detection file
-"""
-import pytest
-from scrape_images import scrape_from_imagenet
-
-def test_scrape_from_imagenet():
-    scrape_from_imagenet("Aubergine", 2, "http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07713074")
-    assert True
